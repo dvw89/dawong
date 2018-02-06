1f873e35de2ec3253f272a56d0abc2a5b80e22e9
David Wong <dawong@csumb.edu>
2018-02-06 23:37:23 +0000

    Homework 1 1st attempt.

---
+++ labs/lab2/index.php
@@ -0,0 +1,13 @@
+<?php include 'inc/functions.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine </title>
+    </head>
+    <body>
+
+        <?php
+            play();
+        ?>
+    </body>
+</html>
